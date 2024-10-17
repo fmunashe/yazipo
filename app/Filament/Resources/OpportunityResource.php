92a1f88db294b615bd246147783da206b080024e
Farai Zihove <farai.zihove@econet.co.zw>
2024-10-18 00:22:15 +0200

    add article and locations APIs

---
+++ app/Filament/Resources/OpportunityResource.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OpportunityResource\Pages;
+use App\Filament\Resources\OpportunityResource\RelationManagers;
+use App\Models\Opportunity;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+
+class OpportunityResource extends Resource
+{
+    protected static ?string $model = Opportunity::class;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Toggle::make('status')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('district_id')
+                    ->searchable()
+                    ->preload()
+                    ->relationship('district', 'name')
+                    ->required(),
+                Forms\Components\Select::make('province_id')
+                    ->searchable()
+                    ->preload()
+                    ->relationship('province', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('dueDate')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('country_code')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('ZW'),
+                Forms\Components\TextInput::make('url'),
+                Forms\Components\TextInput::make('postImageUrl'),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('dueDate')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('country_code')
+                    ->label('Country')
+                    ->getStateUsing(fn($record) => asset("images/flags/{$record->country_code}.svg")),
+
+                Tables\Columns\IconColumn::make('status')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                    ExportBulkAction::make()
+                        ->exports([
+                            ExcelExport::make()
+                                ->askForFilename()
+                                ->askForWriterType()
+                        ])
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOpportunities::route('/'),
+            'create' => Pages\CreateOpportunity::route('/create'),
+            'view' => Pages\ViewOpportunity::route('/{record}'),
+            'edit' => Pages\EditOpportunity::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Content Configurations';
+    }
+}
---
+++ app/Filament/Resources/OpportunityResource/Pages/EditOpportunity.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\OpportunityResource\Pages;
+
+use App\Filament\Resources\OpportunityResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditOpportunity extends EditRecord
+{
+    protected static string $resource = OpportunityResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
